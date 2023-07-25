8059d299ba1b35130326c3a3c4be52b795b8a760
aditya-gupta2106 <adityagupta2106@gmail.com>
2023-07-25 10:08:15 +0530

    All Files for React JS uploaded

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [data, setData] = useState(0);
+
+  function getQuote(){
+    fetch('https://api.quotable.io/random').then(
+      response => response.json()).then(
+        (quote)=>{
+          setData(quote)
+        }
+      )
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="Head">Random Quote Generator</h1>
+        <h3 className="Quote">{data.content}</h3>
+        <h4 className="Author">~{data.author}</h4>
+        <button className="Button" onClick={getQuote}>Get Quote</button>
+        <br/>
+        <br/>
+        <p>Made by Aditya Gupta</p>
       </header>
     </div>
   );
